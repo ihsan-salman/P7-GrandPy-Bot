734286efa9a58b627cb848f047f5ad08c2183ce3
ihsan-salman <ihsan.salmantr@gmail.com>
2021-05-12 10:02:13 +0000

    flask data get

--- GPbot_app/view.py
+++ GPbot_app/view.py
@@ -2,19 +2,14 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
-    return jsonify({
-    	"name": "ihsan"
-    	})
+	return render_template("index.html")
+	if request.method == 'POST':
+		msg = request.form.get('text_input')
+		print(msg)
+
+
 
 #if __name__ == "__main__":
 #   app.run()
-
-# Config options
-app.config.from_object('config')
-
-app.config['SECRET_KEY']
-
