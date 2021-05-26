5f9bfc98b64d6a82c222a194fa5caf942de6e5cc
ihsan-salman <ihsan.salmantr@gmail.com>
2021-05-26 09:20:05 +0000

    pytest coding

--- test_file.py
+++ test_file.py
@@ -1,19 +1,38 @@
-import gpbot_app.helper as helper
+'''!/usr/bin/python3
+   -*- coding: Utf-8 -'''
+
+
 import json
+import gpbot_app.helper as helper
+
 
 def test_parsed():
     '''test the parsed information'''
-    parsed_info = helper.parse('ihsan le la les ils salman')
+    parsed_info = helper.parse('la tour eiffel')
     print(parsed_info)
-    assert parsed_info == 'ihsan salman'
+    assert parsed_info == 'tour eiffel'
+
 
 def test_gmap_response():
-    gmap_adress = helper.gmap_adress('la tour eiffel')
+    '''test the adress function with a parsed information'''
+    gmap_adress = helper.gmap_adress(helper.parse('la tour eiffel'))
     with open('json/gmap.json') as json_data:
         data_dict = json.load(json_data)
     assert data_dict[0]['formatted_address'] == gmap_adress
 
+
 def test_wiki_response():
-    wiki_info = helper.wiki_info('la tour eiffel')
-    print(wiki_info)
-    assert wiki_info == 'La tour Eiffel  est une tour de fer puddlé de 324 mètres de hauteur (avec antennes) située à Paris, à l’extrémité nord-ouest du parc du Champ-de-Mars en bordure de la Seine dans le 7e arrondissement. Son adresse officielle est 5, avenue Anatole-France.'
+    '''test the wikipedia sentences with a parsed infomation'''
+    wiki_info = helper.wiki_info(helper.parse('la tour eiffel'))
+    with open('json/wiki.json') as json_data:
+        data_dict = json.load(json_data)
+    assert wiki_info == data_dict[0]["wiki_info"]
+
+
+def test_gmap_link():
+    '''test the gmap link with an adress'''
+    gmap_link = helper.gmap_link(
+        helper.gmap_adress(helper.parse('la tour eiffel')))
+    with open('json/gmap_link.json') as json_data:
+        data_dict = json.load(json_data)
+    assert gmap_link == data_dict[0]["gmap_link"]
