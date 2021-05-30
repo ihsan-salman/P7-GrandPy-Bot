052f5bbadfaa701ff72bd33c2fa3decb8cec5cb9
ihsan-salman <ihsan.salmantr@gmail.com>
2021-05-30 12:28:26 +0000

    final test for pytest

--- test_file.py
+++ test_file.py
@@ -16,7 +16,7 @@ def test_parsed():
 def test_gmap_response():
     '''test the adress function with a parsed information'''
     gmap_adress = helper.gmap_adress(helper.parse('la tour eiffel'))
-    with open('json/gmap.json') as json_data:
+    with open('gpbot_app/json/gmap.json') as json_data:
         data_dict = json.load(json_data)
     assert data_dict[0]['formatted_address'] == gmap_adress
 
@@ -24,7 +24,7 @@ def test_gmap_response():
 def test_wiki_response():
     '''test the wikipedia sentences with a parsed infomation'''
     wiki_info = helper.wiki_info(helper.parse('la tour eiffel'))
-    with open('json/wiki.json') as json_data:
+    with open('gpbot_app/json/wiki.json') as json_data:
         data_dict = json.load(json_data)
     assert wiki_info == data_dict[0]["wiki_info"]
 
@@ -33,6 +33,19 @@ def test_gmap_link():
     '''test the gmap link with an adress'''
     gmap_link = helper.gmap_link(
         helper.gmap_adress(helper.parse('la tour eiffel')))
-    with open('json/gmap_link.json') as json_data:
+    with open('gpbot_app/json/gmap_link.json') as json_data:
         data_dict = json.load(json_data)
     assert gmap_link == data_dict[0]["gmap_link"]
+
+
+def test_ask_view():
+    '''test the value return by the ask view'''
+    parsed_info = helper.parse('la tour eiffel')
+    gmap_adress = helper.gmap_adress(parsed_info)
+    wiki_info = helper.wiki_info(parsed_info)
+    gmap_link = helper.gmap_link(gmap_adress)
+    json_info = {"parsed_info": parsed_info, "gmap_adress": gmap_adress,
+                 "wiki_info": wiki_info, "gmap_link": gmap_link}
+    with open('gpbot_app/json/view.json') as json_data:
+        data_dict = json.load(json_data)
+    assert json_info == data_dict[0]
